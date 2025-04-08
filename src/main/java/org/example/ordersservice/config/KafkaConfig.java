c6c3303d2d9fe5113dc80a0f0cfc59fa66d465d5
Max <gerasmaks@yandex.ru>
2025-04-09 00:57:44 +0300

    removed Kafka

--- src/main/java/org/example/ordersservice/config/KafkaConfig.java
+++ src/main/java/org/example/ordersservice/config/KafkaConfig.java
@@ -32,46 +32,4 @@ import java.util.Map;
 @EnableKafka
 public class KafkaConfig {
 
-    private final String bootstrapServers = "localhost:9092"; // Или другой адрес вашего Kafka сервера
-    private final String groupId = "my-consumer-group"; // Или другое имя для группы потребителей
-
-    @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
-    }
-
-    @Bean
-    public Map<String, Object> consumerConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        return props;
-    }
-
-    @Bean
-    public MessageListenerContainer messageListenerContainer() {
-        // Убедитесь, что контейнер правильно сконфигурирован
-        MessageListenerContainer container =
-                new ConcurrentMessageListenerContainer<>(consumerFactory(), containerProperties());
-        return container;
-    }
-
-    @Bean
-    public MessageListener<String, String> messageListener() {
-        return new MessageListener<String, String>() {
-            @Override
-            public void onMessage(ConsumerRecord<String, String> record) {
-                System.out.println("Received message: " + record.value());
-                // Логика обработки сообщений из Kafka
-            }
-        };
-    }
-
-    private ContainerProperties containerProperties() {
-        ContainerProperties properties = new ContainerProperties("order-topic");
-        properties.setMessageListener(messageListener());
-        return properties;
-    }
 }
